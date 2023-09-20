4f0c959376a75b5d2f634f73be014a0eacb753e6
Govind <108064331+Govindaraju8@users.noreply.github.com>
2023-09-20 14:06:03 +0530

    Add files via upload

---
+++ BootRegistration/src/main/java/com/BootMVC/BootRegistration/BootRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.BootMVC.BootRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRegistrationApplication.class, args);
+	}
+}
